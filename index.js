93a97df58272f3c7ce73a8de775ffb162607c991
Rinzan Guruvayur <inspirocreates@gmail.com>
2025-04-23 07:03:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ function draw() {
 
   // Draw frame
   ctx.drawImage(document.getElementById("frame"), 0, 0);
-  ctx.fillText(document.getElementById("username").value, 271, 881);
+  ctx.fillText(document.getElementById("username").value, 650, 900);
 }
 
 // downlad function
