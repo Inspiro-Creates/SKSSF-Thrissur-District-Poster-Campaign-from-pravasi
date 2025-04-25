936d652068eb1fdf75d9ba761613c6e9bdca315f
Rinzan Guruvayur <inspirocreates@gmail.com>
2025-04-25 13:45:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function getImage() {
   newImg.id = "myGreatImage";
   // uploadedImageDiv.style.border = "4px solid #FCB514";
   // uploadedImageDiv.innerHTML
-  uploadedImageDiv.style.width = "260px";
-  uploadedImageDiv.style.height = "260px";
+  uploadedImageDiv.style.width = "250px";
+  uploadedImageDiv.style.height = "300px";
   uploadedImageDiv.appendChild(newImg);
   myGreatImage = document.getElementById("myGreatImage");
 
@@ -57,7 +57,7 @@ function processImage() {
     // maxContainerWidth   : 200,
     // maxCanvasWidth      : 200,
     // maxCanvasHeight     : 200,
-    aspectRatio:  260/260,  
+    aspectRatio:  250/300,  
     autoCropArea: 1,
     background: true,
     movable: false,
@@ -111,10 +111,10 @@ function draw() {
   // Draw slice
   ctx.drawImage(
     document.getElementById("croppedImage"),
-    141,
-    652,
-    280,
-    280
+    654,
+    299,
+    250,
+    300
     // 0,
     // 0,
     // 0,
@@ -123,7 +123,7 @@ function draw() {
 
   // Draw frame
   ctx.drawImage(document.getElementById("frame"), 0, 0);
-  ctx.fillText(document.getElementById("username").value, 271, 900);
+  ctx.fillText(document.getElementById("username").value, 787, 664);
 }
 
 // downlad function
